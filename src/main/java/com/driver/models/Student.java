a040dbbb975bd0b9460bebaf527767419a3481c8
Deepanshigreza <deepanshigreza@gmail.com>
2023-01-27 00:53:02 +0530

    done

--- src/main/java/com/driver/models/Student.java
+++ src/main/java/com/driver/models/Student.java
@@ -116,13 +116,11 @@ public class Student {
     }
 
 
-    public Student(String emailId, String name, int age, String country, Card card, Date createdOn, Date updatedOn) {
+    public Student(String emailId, String name, int age, String country) {
         this.emailId = emailId;
         this.name = name;
         this.age = age;
         this.country = country;
-        this.card = card;
-        this.createdOn = createdOn;
-        this.updatedOn = updatedOn;
+
     }
 }
